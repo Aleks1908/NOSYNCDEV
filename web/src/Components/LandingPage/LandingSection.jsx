6fd9b3c43bec39048875dabcbf39dd9677ed05d4
Aleks <al.ivanov1908@gmail.com>
2023-04-01 02:26:58 +0300

    #1 css additions

--- web/src/Components/LandingPage/LandingSection.jsx
+++ web/src/Components/LandingPage/LandingSection.jsx
@@ -4,15 +4,14 @@ import './landing_section.css';
 export const LandingSection = () => {
     return (
         <div className="landing-main">
-            <h1 className="landing-header">Landing Section</h1>
-            <div class="container">
-                <h1>Adventure</h1>
+            <div className="header">
+                <h1 className="landing-header">ADVENTURE</h1>
+            </div>
+            <div className="header-info">
                 <p>There are many variations of passages of Lorem Ipsum available, but the majority have sufferedThere are ma available, but the majority have suffered</p>
-                <div>
-                    <button>
-                        Questionnaire Form
-                    </button>
-                </div>
+            </div>
+            <div className="header-btn"> 
+                <a href="#"> Questionare</a>
             </div>
         </div>
     )
