c02667f2628c1a0d0937135f92b75701e5cef5f9
SuelaCark <suela.qarkaxhija@gmail.com>
2023-04-01 10:20:36 +0300

    #16 added about section

---
+++ web/src/Components/AboutSection/AboutSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './about_section.css';
+
+export const AboutSection = () => {
+    return (
+        <div className="about-main">
+            {/* Create a react card component which will be reused for adding cards about facts*/}
+            {/* Could be able to implement smth like the Jobs section without the making requests to the admin panel (hardcode in instead) */}
+        </div>
+    )
+}
+ 
+export default AboutSection;
