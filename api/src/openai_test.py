dfd860b87875b48a9850996f264ea4dce0677cde
Daniel <torta979@gmail.com>
2023-04-01 14:13:32 +0300

    separating the answer from openai into two list of strings, the first one with the city, country values and the second with the descriptions. IMPPORTANT: the city's index from the first list corresponds to the same index in the second list

--- api/src/openai_test.py
+++ api/src/openai_test.py
@@ -18,13 +18,36 @@ openai.organization = "org-U5BcpPCYe6UAevd1ylf5SuDP"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
-    messages=add_questions()
-    # messages=[
-    #     {"role": "user", "content": "Who won the world series in 2020?"},
-    #     {"role": "user", "content": "Where was it played?"},
-    #     {"role": "user", "content": "Who was on second place!"}
-    # ]
+    # messages=add_questions()
+    messages=[
+        {"role": "user", "content": "Suggest 5 cities with cold climate, luxury accommodation, modern attractions, natural environment, which are adventure-packed, which have airport. Return the cities in the format 'city, country':'short description'"},
+        #{"role": "user", "content": "Where was it played?"},
+        #{"role": "user", "content": "Who was on second place!"}
+    ]
 )
 
+api_response = completion.choices[0].message['content']
 
-print(completion.choices[0].message['content'])
+#city_country, description = api_response.split(":")
+#list = [x for x in raw_list if x != "\n"]
+#print(city_country)
+#print(description)
+
+def split_data(api_response_data):
+    raw_list = api_response.split("\n")
+
+    city_country_list = []
+    descriptions_list = []
+
+    for element in raw_list:
+        if element == "":
+            raw_list.remove(element)
+    print(raw_list)
+    for place_description in raw_list:
+        city_country_list.append(place_description.split(": ")[0])
+        descriptions_list.append(place_description.split(": ")[1])
+
+    print(city_country_list)
+    print(descriptions_list)
+
+split_data(api_response)
