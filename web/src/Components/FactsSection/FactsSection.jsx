735e04f58c331106f977d186c411f972af8dfcb2
SuelaCark <suela.qarkaxhija@gmail.com>
2023-04-02 00:56:46 +0300

    fixed facts section images

--- web/src/Components/FactsSection/FactsSection.jsx
+++ web/src/Components/FactsSection/FactsSection.jsx
@@ -8,21 +8,21 @@ export const FactsSection = (props) => {
     return (
         <div className="facts-main-container">
             <h1 className="facts-header">Featured places</h1>
-            <div className="facts-components">
+            <div className="facts-components">   
                 <FactCard
-                    logo={'scuba-diving.jpg'}
-                    title={'Scuba diving in Indonesia'}
-                    text={'Indonesia is known for scuba diving due to its beautiful oceans.'}
-                />   
-                <FactCard
-                    logo={'scuba-diving.jpg'}
-                    title={'Scuba diving in Indonesia'}
-                    text={'Indonesia is known for scuba diving due to its beautiful oceans.'}
+                    logo={'pizza-in-italy.avif'}
+                    title={'Famous Italian pizzas'}
+                    text={'Pizzas were invented in Naples, Italy in the 18th century.'}
                 /> 
                 <FactCard
                     logo={'scuba-diving.jpg'}
                     title={'Scuba diving in Indonesia'}
                     text={'Indonesia is known for scuba diving due to its beautiful oceans.'}
+                />
+                <FactCard
+                    logo={'rock-climbing-greece.jpg'}
+                    title={'Rock climbing in Greece'}
+                    text={'Greece is known for its beautiful rock climbing sights and experiences.'}
                 /> 
             </div>
         </div>
