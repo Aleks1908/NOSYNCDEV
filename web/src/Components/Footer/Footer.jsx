93c1bc324873834e809e74e0fdc11086b7f4e410
SuelaCark <suela.qarkaxhija@gmail.com>
2023-04-01 17:38:21 +0300

    #8 added footer structure and design

---
+++ web/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <p>© 2023 Journo. All rights reserved</p>
+        </div>
+    );
+};
+
+export default Footer;
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import LandingSection from './Components/LandingPage/LandingSection';
 import QForm from './Components/QForm/QForm';
 import AboutSection from './Components/AboutSection/AboutSection';
+import Footer from './Components/Footer/Footer';
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <LandingSection/>
   <AboutSection/>
   <QForm/>
+  <Footer/>
   </React.StrictMode>,
 )
