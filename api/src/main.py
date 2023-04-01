53288e803a756d4a75466246bc72f5e894aa0a04
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-04-01 18:54:58 +0300

    18 fastapi showcities endpoints (#21)
    * Create showcites endpoint
    
    * Create request towards OpenAI via the showcities endpoint
    
    Clean up code
    
    * Add new libs
    
    * Finish city questions in showcities endpoint

--- api/src/main.py
+++ api/src/main.py
@@ -1,1 +1,37 @@
-print("HELLO WORLD")
+from __future__ import annotations
+
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
+from starlette.responses import JSONResponse
+
+from openai_test import ask_openai
+
+app = FastAPI()
+
+
+def create_openai_request_message(json_data):
+    """Creates the request message for OpenAI with values from response_body"""
+    climate = json_data["climate"]
+    accommodation = json_data["accommodation"]
+    environment_type = json_data["environment_type"]
+    trip_type = json_data["trip_type"]
+
+
+    str_message = f"Suggest 5 cities with {climate} climate, {accommodation} accommodation, {environment_type} environment, which are {trip_type}, which have an airport. Return the cities in the format 'city, country':'short description'"
+
+    return str_message
+
+@app.post("/showcities/")
+async def return_cities(request: Request):
+
+    # Gets the request body as JSON
+    json_data = await request.json()
+
+    openai_request_message = create_openai_request_message(json_data)
+
+    # OpenAI returns two list one with cities and the other with descriptions
+    city_country_list, descriptions_list = ask_openai(openai_request_message)
+
+    return JSONResponse(content=jsonable_encoder({"message": "Data was sent to OpenAI. Here is the response",
+                                                  "city_country_list":city_country_list,
+                                                  "descriptions_list": descriptions_list }))
--- api/src/openai_test.py
+++ api/src/openai_test.py
@@ -1,39 +1,25 @@
 import os
 import openai
 from dotenv import load_dotenv
-def add_questions():
-    questions_answers_dict = {}
-    messages = []
-    # TODO: Fill dict with questions for keys and inputs from front-end for values
-    #  - The idea is to create sentence or sentences from those answers to pass to content as a variable
 
-    # This is how a dict should like where content is a single message. This should be appended to messages list
-    # {"role": "user", "content": "Some text"},
 
-    return messages
+def ask_openai(request_message):
+    load_dotenv()
 
-load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user","content": request_message}]
+    )
 
-openai.organization = "org-U5BcpPCYe6UAevd1ylf5SuDP"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    # messages=add_questions()
-    messages=[
-        {"role": "user", "content": "Suggest 5 cities with cold climate, luxury accommodation, modern attractions, natural environment, which are adventure-packed, which have airport. Return the cities in the format 'city, country':'short description'"},
-        #{"role": "user", "content": "Where was it played?"},
-        #{"role": "user", "content": "Who was on second place!"}
-    ]
-)
+    api_response = completion.choices[0].message['content']
 
-api_response = completion.choices[0].message['content']
+    city_country_list, descriptions_list = split_data(api_response)
 
-#city_country, description = api_response.split(":")
-#list = [x for x in raw_list if x != "\n"]
-#print(city_country)
-#print(description)
+    return city_country_list, descriptions_list
 
-def split_data(api_response_data):
+
+def split_data(api_response):
     raw_list = api_response.split("\n")
 
     city_country_list = []
@@ -49,5 +35,6 @@ def split_data(api_response_data):
 
     print(city_country_list)
     print(descriptions_list)
+    return city_country_list, descriptions_list
+    #TODO: return lists
 
-split_data(api_response)
