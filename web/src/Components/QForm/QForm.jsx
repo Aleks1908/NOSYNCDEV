749017cf53be7c242002c3a3a0084ad226dd52ca
Aleks <al.ivanov1908@gmail.com>
2023-04-01 14:50:56 +0300

    #3 fix variables

--- web/src/Components/QForm/QForm.jsx
+++ web/src/Components/QForm/QForm.jsx
@@ -42,7 +42,6 @@ const QForm = () => {
     useEffect(checkButtonAvailability, [Object.keys(errors)]);
 
   return (
-    //(onSubmit) on next line
     <div className="registration-main" id="registration">
         <h1>Questionare</h1>
         <form 
@@ -50,36 +49,175 @@ const QForm = () => {
                 className="reg-form"
         >
             <fieldset className="from-personal-info">
-                
                 <div className="send-info">
                     <label>
-                        School/University
+                    What climate environment do you prefer? 
+
                         <select
                             defaultValue=""
                             className="select"
-                            {...register('university', {
+                            {...register('climate', {       //variables!!!!!!!!!
                                 required: true
                             })}
                         >
                             <option value="" disabled>
-                                Choose an School/University
+                                Choose climate
                             </option>
-                            <option value="AUBG">AUBG</option>
-                            <option value="Sofia University">Sofia University</option>
-                            <option value="Technical University - Sofia">Technical University - Sofia</option>
-                            <option value="Plovdiv University">Plovdiv University</option>
-                            <option value="Other">Other</option>
+                            <option value="Warm">Warm</option>
+                            <option value="Cold">Cold</option>
                         </select>
                     </label>
-                    {errors.university && (
+                    {errors.climate && (             //variables!!!!
                         <p className="error-text">
-                            *University/School is required
+                            *This field is required
                         </p>
                     )}
                 </div>
+                <div className="send-info">
+                    <label>
+                    What accommodation do you look for?
 
+                        <select
+                            defaultValue=""
+                            className="select"
+                            {...register('accommodation', {       //variables!!!!!!!!!
+                                required: true
+                            })}
+                        >
+                            <option value="" disabled>
+                                Choose one option
+                            </option>
+                            <option value="High-End">High-End</option>
+                            <option value="Budget">Budget</option>
+                        </select>
+                    </label>
+                    {errors.accommodation && (                //variables!!!!!
+                        <p className="error-text">
+                            *This field is required
+                        </p>
+                    )}
+                </div>
+                <div className="send-info">
+                    <label>
+                    What city do you prefer?
 
-                
+                        <select
+                            defaultValue=""
+                            className="select"
+                            {...register('city_preference', {       //variables!!!!!!!!!
+                                required: true
+                            })}
+                        >
+                            <option value="" disabled>
+                                Choose city type
+                            </option>
+                            <option value="Historic">Historic</option>
+                            <option value="Cultural">Cultural</option>
+                            <option value="Modern">Modern</option>
+                        </select>
+                    </label>
+                    {errors.city_preference && (             //variables!!!!
+                        <p className="error-text">
+                            *This field is required
+                        </p>
+                    )}
+                </div>
+                <div className="send-info">
+                    <label>
+                    What type of trip are you planning?
+
+                        <select
+                            defaultValue=""
+                            className="select"
+                            {...register('trip_plan', {       //variables!!!!!!!!!
+                                required: true
+                            })}
+                        >
+                            <option value="" disabled>
+                                Choose trip type
+                            </option>
+                            <option value="Leisure">Leisure</option>
+                            <option value="Adventure">Adventure</option>
+                        </select>
+                    </label>
+                    {errors.trip_plan && (             //variables!!!!
+                        <p className="error-text">
+                            *This field is required
+                        </p>
+                    )}
+                </div>
+                <div className="send-info">
+                    <label>
+                    What attractions do you prefer to visit?
+
+                        <select
+                            defaultValue=""
+                            className="select"
+                            {...register('attractions', {       //variables!!!!!!!!!
+                                required: true
+                            })}
+                        >
+                            <option value="" disabled>
+                                Choose one option
+                            </option>
+                            <option value="Urban">Urban</option>
+                            <option value="Natural">Natural</option>
+                        </select>
+                    </label>
+                    {errors.attractions && (             //variables!!!!
+                        <p className="error-text">
+                            *This field is required
+                        </p>
+                    )}
+                </div>
+                <div className="send-info">
+                    <label>
+                    What cultural immersion do you prefer?
+
+                        <select
+                            defaultValue=""
+                            className="select"
+                            {...register('immersion', {       //variables!!!!!!!!!
+                                required: true
+                            })}
+                        >
+                            <option value="" disabled>
+                                Choose one option
+                            </option>
+                            <option value="Touristy">Touristy</option>
+                            <option value="Off-the-beaten-track">Off-the-beaten-track</option>
+                        </select>
+                    </label>
+                    {errors.immersion && (             //variables!!!!
+                        <p className="error-text">
+                            *This field is required
+                        </p>
+                    )}
+                </div>
+                <div className="send-info">
+                    <label>
+                    What are your food preferences
+
+                        <select
+                            defaultValue=""
+                            className="select"
+                            {...register('food', {       //variables!!!!!!!!!
+                                required: true
+                            })}
+                        >
+                            <option value="" disabled>
+                                Choose city type
+                            </option>
+                            <option value="Historic">Local cuisine</option>
+                            <option value="International-cuisine">International cuisine</option>
+                        </select>
+                    </label>
+                    {errors.food && (             //variables!!!!
+                        <p className="error-text">
+                            *This field is required
+                        </p>
+                    )}
+                </div>
             </fieldset>
             <input
             type="submit"
