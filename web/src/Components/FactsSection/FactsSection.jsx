02c260d83f0e4fb16b4c3718bd721ff9a500c847
SuelaCark <suela.qarkaxhija@gmail.com>
2023-04-01 22:01:54 +0300

    fixed design structure of featured places

--- web/src/Components/FactsSection/FactsSection.jsx
+++ web/src/Components/FactsSection/FactsSection.jsx
@@ -6,17 +6,25 @@ export const FactsSection = (props) => {
     const {children} = props
 
     return (
-        <div className="facts-container">
-            <FactCard
-                logo={'scuba-diving.jpg'}
-                title={'Scuba diving in Indonesia'}
-                text={'Indonesia is known for scuba diving due to its beautiful oceans.'}
-            />   
-            <FactCard
-                logo={'scuba-diving.jpg'}
-                title={'Scuba diving in Indonesia'}
-                text={'Indonesia is known for scuba diving due to its beautiful oceans.'}
-            />                       
+        <div className="facts-main-container">
+            <h1 className="facts-header">Featured places</h1>
+            <div className="facts-components">
+                <FactCard
+                    logo={'scuba-diving.jpg'}
+                    title={'Scuba diving in Indonesia'}
+                    text={'Indonesia is known for scuba diving due to its beautiful oceans.'}
+                />   
+                <FactCard
+                    logo={'scuba-diving.jpg'}
+                    title={'Scuba diving in Indonesia'}
+                    text={'Indonesia is known for scuba diving due to its beautiful oceans.'}
+                /> 
+                <FactCard
+                    logo={'scuba-diving.jpg'}
+                    title={'Scuba diving in Indonesia'}
+                    text={'Indonesia is known for scuba diving due to its beautiful oceans.'}
+                /> 
+            </div>
         </div>
     )
 }
