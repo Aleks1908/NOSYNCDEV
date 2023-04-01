ccf650defa9b81ed0ff262fb0c2ed178fef1f8db
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-04-01 15:53:01 +0300

    Create request towards OpenAI via the showcities endpoint
    Clean up code

--- api/src/main.py
+++ api/src/main.py
@@ -2,24 +2,32 @@ from __future__ import annotations
 
 from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
-from jsonschema import FormatChecker, validate
-
-from pydantic import BaseModel
 from starlette.responses import JSONResponse
+
+from openai_test import ask_openai
 
 app = FastAPI()
 
-def create_openai_request_api(json_data):
+def create_openai_request_message(json_data):
+    """Creates the request message for OpenAI with values from response_body"""
+    str_message = "Suggest 5 cities with cold climate, luxury accommodation, modern attractions, natural environment, which are adventure-packed, which have airport. Return the cities in the format 'city, country':'short description'"
     destination = json_data["destination"]
     climate = json_data["climate"]
 
     message = destination + " " + climate
-    return message
+    return str_message
 
 @app.post("/showcities/")
 async def return_cities(request: Request):
-    json_data = await request.json()
-    openai_request_message = create_openai_request_api(json_data)
-    # TODO: call openai and pass message
 
-    return JSONResponse(content=jsonable_encoder({"message": "Data was sent to OpenAI", "data":openai_request_message}))
+    # Gets the request body as JSON
+    json_data = await request.json()
+
+    openai_request_message = create_openai_request_message(json_data)
+
+    # OpenAI returns two list one with cities and the other with descriptions
+    city_country_list, descriptions_list = ask_openai(openai_request_message)
+
+    return JSONResponse(content=jsonable_encoder({"message": "Data was sent to OpenAI. Here is the response",
+                                                  "city_country_list":city_country_list,
+                                                  "descriptions_list": descriptions_list }))
