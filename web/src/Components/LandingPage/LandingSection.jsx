4b979f164971a4a20301366475963d4a02c326df
Aleks <al.ivanov1908@gmail.com>
2023-04-01 03:02:08 +0300

    #1 dimmer added

--- web/src/Components/LandingPage/LandingSection.jsx
+++ web/src/Components/LandingPage/LandingSection.jsx
@@ -4,14 +4,16 @@ import './landing_section.css';
 export const LandingSection = () => {
     return (
         <div className="landing-main">
-            <div className="header">
-                <h1 className="landing-header">ADVENTURE</h1>
-            </div>
-            <div className="header-info">
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have sufferedThere are ma available, but the majority have suffered</p>
-            </div>
-            <div className="header-btn"> 
-                <a href="#"> Questionare</a>
+            <div className="dimmer">
+                <div className="header">
+                    <h1 className="landing-header">ADVENTURE</h1>
+                </div>
+                <div className="header-info">
+                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have sufferedThere are ma available, but the majority have suffered</p>
+                </div>
+                <div className="header-btn"> 
+                    <a href="#"> Questionare</a>
+                </div>
             </div>
         </div>
     )
