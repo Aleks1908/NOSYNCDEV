cbd77e35ac3a165e0fa9cd5c01b1eac9d5986021
SuelaCark <suela.qarkaxhija@gmail.com>
2023-04-01 09:00:43 +0300

    #15 added the landing text animation

--- web/src/Components/LandingPage/LandingSection.jsx
+++ web/src/Components/LandingPage/LandingSection.jsx
@@ -8,7 +8,7 @@ export const LandingSection = () => {
             <div className="dimmer">
                 <Navbar/>
                 <div className="header">
-                    <h1 className="landing-header">ADVENTURE</h1>
+                    <h1 className="landing-header"><span id="spin"></span></h1>
                 </div>
                 <div className="header-info">
                     <p>There are many variations of passages of Lorem Ipsum available, but the majority have sufferedThere are ma available, but the majority have suffered</p>
