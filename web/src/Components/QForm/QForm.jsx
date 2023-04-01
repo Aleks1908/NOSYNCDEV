1439df1450e7464813bb7ad2c214fe73cc017e59
Aleks <al.ivanov1908@gmail.com>
2023-04-01 19:07:37 +0300

    #3 card design progress

--- web/src/Components/QForm/QForm.jsx
+++ web/src/Components/QForm/QForm.jsx
@@ -5,10 +5,11 @@ import { useEffect } from "react";
 import './QForm.css'
 
 const QForm = () => {
-
+    const [formStep, setFormStep] = React.useState(0);
     const onSubmit = (data) => {
         console.log(data)
     };
+
 
     const { register, 
         handleSubmit, 
@@ -32,13 +33,16 @@ const QForm = () => {
         } else if (Object.keys(errors).length == 0 && submitPressed) {
             setButtonState('hackaubg-register-btn');
             setSubmitButtonValue('Success');
-
+            setFormStep(formStep + 1);
+            console.log(Object.keys(errors));
+            setSubmitPressed(false);
+            
             return;
         }
         setButtonState('hackaubg-register-btn');
         setSubmitButtonValue('Submit');
     };
-
+    console.log(formStep)
     useEffect(checkButtonAvailability, [Object.keys(errors)]);
 
   return (
@@ -48,7 +52,7 @@ const QForm = () => {
                 onSubmit={handleSubmit(onSubmit)} 
                 className="reg-form"
         >
-            <fieldset className="from-personal-info">
+            {formStep === 0 && (<fieldset className="from-personal-info">
                 <div className="send-info">
                     <label>
                         <p>I want to go to a </p>
@@ -69,124 +73,7 @@ const QForm = () => {
                         <p>place. </p>
                     </label>
                 </div>
-                <div className="send-info">
-                    <label>
-                    <p>I prefer a(n) </p>
-
-                        <select
-                            defaultValue=""
-                            className="select"
-                            {...register('accommodation', {       //variables!!!!!!!!!
-                                required: true
-                            })}
-                        >
-                            <option value="" disabled>
-                                
-                            </option>
-                            <option value="Luxury">Luxury</option>
-                            <option value="Affordable">Affordable</option>
-                        </select>
-                        <p> accommodation. </p>
-                    </label>
-                </div>
-                <div className="send-info">
-                    <label>
-                    <p>I am more interested in </p>
-
-                        <select
-                            defaultValue=""
-                            className="select"
-                            {...register('city_preference', {       //variables!!!!!!!!!
-                                required: true
-                            })}
-                        >
-                            <option value="" disabled>
-                            </option>
-                            <option value="Historic sites">Historic sites</option>
-                            <option value="Modern attractions">Modern attractions</option>
-                        </select>
-                        <p>.</p>
-                    </label>
-                </div>
-                <div className="send-info">
-                    <label>
-                    <p>I am into </p>
-
-                        <select
-                            defaultValue=""
-                            className="select"
-                            {...register('trip_plan', {       //variables!!!!!!!!!
-                                required: true
-                            })}
-                        >
-                            <option value="" disabled>
-                            </option>
-                            <option value="Leisure trip">Leisure</option>
-                            <option value="Adventures trip">Adventures</option>
-                        </select>
-                        <p> trips. </p>
-                    </label>
-                </div>
-                <div className="send-info">
-                    <label>
-                    <p>I would rather visit </p>
-
-                        <select
-                            defaultValue=""
-                            className="select"
-                            {...register('attractions', {       //variables!!!!!!!!!
-                                required: true
-                            })}
-                        >
-                            <option value="" disabled>
-                            </option>
-                            <option value="Urban places">Urban</option>
-                            <option value="Natural environment">Natural</option>
-                        </select>
-                        <p> destination. </p>
-                    </label>
-                </div>
-                <div className="send-info">
-                    <label>
-                    
-
-                        <select
-                            defaultValue=""
-                            className="select"
-                            {...register('immersion', {       //variables!!!!!!!!!
-                                required: true
-                            })}
-                        >
-                            <option value="" disabled>
-                                
-                            </option>
-                            <option value="Touristy destination">A touristy destination</option>
-                            <option value="Off-the-beaten-path">Off-the-beaten-path</option>
-                        </select>
-                        <p> is my way to go. </p>
-                    </label>
-                </div>
-                <div className="send-info">
-                    <label>
-                    <p>I would prefer eating </p>
-
-                        <select
-                            defaultValue=""
-                            className="select"
-                            {...register('food', {       //variables!!!!!!!!!
-                                required: true
-                            })}
-                        >
-                            <option value="" disabled>
-                            </option>
-                            <option value="Local cuisine">Local cuisine</option>
-                            <option value="International-cuisine">International cuisine</option>
-                        </select>
-                        <p> .</p>
-                    </label>
-                </div>
-            </fieldset>
-            <input
+                <input
             type="submit"
             className={buttonState}
             value={submitButtonValue}
@@ -194,9 +81,38 @@ const QForm = () => {
                 setSubmitPressed(true);
             }}
                 />
+            </fieldset>)}
+            {formStep === 1 && (<fieldset className="from-personal-info">
+                <div className="send-info">
+                    <label>
+                        <p>BRAAAT</p>
+                        <select
+                            defaultValue=""
+                            className="select"
+                            {...register('climate', {       //variables!!!!!!!!!
+                                required: true
+                            })}
+                        >
+                            <option value="" disabled>
+                            </option>
+                            <option value="Warm climate">Warm</option>
+                            <option value="Cold climate">Cold</option>
+                        
+                        </select>
+                        <p>place. </p>
+                    </label>
+                </div>
+                <input
+            type="submit"
+            className={buttonState}
+            value={submitButtonValue}
+            onClick={() => {
+                setSubmitPressed(true);
+            }}
+                />
+            </fieldset>)}            
         </form>
     </div>
   );
 }
- 
  export default QForm;
