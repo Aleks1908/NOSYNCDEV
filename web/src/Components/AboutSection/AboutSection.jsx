9a4372ebbc456a2b0f5820d922a4593f50aaece9
SuelaCark <suela.qarkaxhija@gmail.com>
2023-04-01 15:35:01 +0300

    #7 added about us section structure and fixed navbar

--- web/src/Components/AboutSection/AboutSection.jsx
+++ web/src/Components/AboutSection/AboutSection.jsx
@@ -4,7 +4,20 @@ import './about_section.css';
 export const AboutSection = () => {
     return (
         <div className="about-main">
-            <h1>About</h1>
+            <div className="about-text">
+                <h1>About us</h1>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration 
+                    in some form, by injected humour, or randomised words which don't look even slightly believable. If you 
+                    are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden 
+                    in the middle of text. 
+                </p>
+                {/* <div className="landing-btn"> 
+                    <a href="#">Read more</a>
+                </div> */}
+            </div>
+            <div className="about-picture">
+                <img src="././public/about-picture.jpg" alt="Traveling around the world" />
+            </div>
         </div>
     )
 }
--- web/src/Components/NavBar/NavBar.jsx
+++ web/src/Components/NavBar/NavBar.jsx
@@ -5,42 +5,6 @@ import NavMobile from "./NavMobile";
 import "./navbar.css";
 
 const Navbar = ({ props }) => {
-    // const NavDesktop = () => {
-    //     return (
-    //         <div className="navbar-main">
-    //             <div>
-    //                 <img src="../../../journo-logo.png" alt="" />
-    //             </div>
-    //             <div className="navbar-buttons">
-    //                 <a href="#">About</a>
-    //                 <a href="#">Questionnaire</a>
-    //                 <a href="#">Results</a>
-    //                 <a href="#">Contact us</a>
-    //             </div>
-    //         </div>
-    //     );
-    // }
-
-    // const NavMobile = () => {
-    //     return (
-    //         <div className="navbar-main">
-    //             <div className="journo-logo">
-    //                 <img src="../../../journo-logo.png" alt="" />
-    //                 <a href="" className="icon" onclick="myFunction()">
-    //                 <i className="fa fa-bars"></i>
-    //             </a>
-    //             </div>
-    //             {/* <div className="navbar-buttons">
-    //                 <a href="#">About</a>
-    //                 <a href="#">Questionnaire</a>
-    //                 <a href="#">Results</a>
-    //                 <a href="#">Contact us</a>
-    //             </div> */}
-    //             
-    //         </div>
-    //     );
-    // }
-
     const isMobile = useMediaQuery({ query: '(max-width: 900px)' });
 
     if (!isMobile) {
