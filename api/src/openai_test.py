9d179542e5c4fbdc77636abba0c7b6154cac67ea
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-04-01 05:00:11 +0300

    Add TODOS for add_questions func which returns a list of dict that is passed to OpenAI

--- api/src/openai_test.py
+++ api/src/openai_test.py
@@ -1,16 +1,30 @@
 import os
 import openai
 from dotenv import load_dotenv
+def add_questions():
+    questions_answers_dict = {}
+    messages = []
+    # TODO: Fill dict with questions for keys and inputs from front-end for values
+    #  - The idea is to create sentence or sentences from those answers to pass to content as a variable
+
+    # This is how a dict should like where content is a single message. This should be appended to messages list
+    # {"role": "user", "content": "Some text"},
+
+    return messages
 
 load_dotenv()
 
 openai.organization = "org-U5BcpPCYe6UAevd1ylf5SuDP"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": "How are you!"}
-  ]
+    model="gpt-3.5-turbo",
+    messages=add_questions()
+    # messages=[
+    #     {"role": "user", "content": "Who won the world series in 2020?"},
+    #     {"role": "user", "content": "Where was it played?"},
+    #     {"role": "user", "content": "Who was on second place!"}
+    # ]
 )
 
-print(completion.choices[0].message)
+
+print(completion.choices[0].message['content'])
