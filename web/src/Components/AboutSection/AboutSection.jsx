1e86337e6ddcc88ba2ece237e6fcdf79faf93133
SuelaCark <suela.qarkaxhija@gmail.com>
2023-04-02 12:20:11 +0300

    added final website text

--- web/src/Components/AboutSection/AboutSection.jsx
+++ web/src/Components/AboutSection/AboutSection.jsx
@@ -6,10 +6,10 @@ export const AboutSection = () => {
         <div className="about-main" id="About">
             <div className="about-text">
                 <h1>About us</h1>
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration 
-                    in some form, by injected humour, or randomised words which don't look even slightly believable. If you 
-                    are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden 
-                    in the middle of text. 
+                <p>Welcome to our travel booking website - where adventure awaits and time is on your side. 
+                    With our easy-to-use platform, you can quickly search and book your next trip without any hassle. 
+                    We offer a wide range of travel options to suit every style and budget, and our 24/7 customer 
+                    support is always here to help. So why wait? Let us help you turn your travel dreams into a reality.
                 </p>
             </div>
             <div className="about-picture">
